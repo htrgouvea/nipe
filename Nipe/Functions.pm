113eb81453ce9f125dd0bc8eee9302fcdbd2fd13
GouveaHeitor <hi@heitorgouvea.me>
2016-06-22 13:55:34 -0300

    :alien: add perl and iptables packages at install

--- Nipe/Functions.pm
+++ Nipe/Functions.pm
@@ -25,22 +25,22 @@ sub help {
 
 sub install {
 	if (($os =~ /[U,u]buntu/) || ($os =~ /[D,d]ebian/)) {
-		system ("sudo apt-get install tor");
+		system ("sudo apt-get install tor perl iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/ubuntu/torrc");
 	}
 
 	elsif ($os =~ /[A,a]rch/) {
-		system ("sudo pacman -S tor");
+		system ("sudo pacman -S tor perl iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/arch/torrc");
 	}
 
 	elsif ($os =~ /[F,f]edora/) {
-		system ("sudo dnf install tor");
+		system ("sudo dnf install tor perl iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/fedora/torrc");
 	}
 
 	else {
-		system ("sudo pacman -S tor");
+		system ("sudo pacman -S tor perl iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/arch/torrc");
 	}
 

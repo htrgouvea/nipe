d0cf2aeafe24f1bf2703eb3ad9193e6dd10a8d6a
HeitorG <hi@heitorgouvea.me>
2016-06-29 22:59:50 -0300

    new directory to libs :camel:

--- nipe.pl
+++ nipe.pl
@@ -13,6 +13,7 @@
 #########################################################
 
 use Switch;
+use lib "./lib/";
 use Nipe::Stop;
 use Nipe::Start;
 use Nipe::Check;
@@ -23,7 +24,7 @@ my $command = $ARGV[0];
 print "\n\033[1;32m
 88b 88   88   8888Yb  888888     
 88Yb88   88   88__dP  88__    Developed by 
-88 Y88   88   88--    88--    Heitor Gouvea (D3LET)  
+88 Y88   88   88--    88--    Heitor 'D3LET' Gouvea
 88  Y8   88   88      888888\n\033[1;37m\n";
 
 switch ($command) {

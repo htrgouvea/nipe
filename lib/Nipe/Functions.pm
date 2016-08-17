f1c532354cc12a942c0c7c15f4e0060215a04a3c
Heitor Gouvêa <hi@heitorgouvea.me>
2016-08-17 13:00:30 -0300

    Update Functions.pm

--- lib/Nipe/Functions.pm
+++ lib/Nipe/Functions.pm
@@ -25,22 +25,22 @@ sub help {
 
 sub install {
 	if (($os =~ /[U,u]buntu/) || ($os =~ /[D,d]ebian/)) {
-		system ("sudo apt-get install tor perl iptables");
+		system ("sudo apt-get install tor iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/ubuntu/torrc");
 	}
 
 	elsif ($os =~ /[A,a]rch/) {
-		system ("sudo pacman -S tor perl iptables");
+		system ("sudo pacman -S tor iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/arch/torrc");
 	}
 
 	elsif ($os =~ /[F,f]edora/) {
-		system ("sudo dnf install tor perl iptables");
+		system ("sudo dnf install tor iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/fedora/torrc");
 	}
 
 	else {
-		system ("sudo pacman -S tor perl iptables");
+		system ("sudo pacman -S tor iptables");
 		system ("sudo wget http://heitorgouvea.me/nipe/arch/torrc");
 	}
 

163d49a420d3a70d0ffb118d892351fd0666bd8c
GouveaHeitor <hgouvea@protonmail.com>
2017-08-04 21:04:46 -0300

    remove infos about the autor

--- lib/Nipe/Start.pm
+++ lib/Nipe/Start.pm
@@ -1,19 +1,5 @@
 #!/usr/bin/perl
 
-#########################################################
-# Nipe developed by Heitor Gouvêa                       #
-# This work is licensed under MIT License               #
-# Copyright (c) 2015-2017 Heitor Gouvêa                 #
-#                                                       #
-# [+] AUTOR:        Heitor Gouvêa                       #
-# [+] EMAIL:        hi@heitorgouvea.me                  #
-# [+] SITE:         http://heitorgouvea.me              #
-# [+] GITHUB:       https://github.com/GouveaHeitor     #
-# [+] TWITTER:      https://twitter.com/GouveaHeitor    #
-# [+] FACEBOOK:     https://fb.com/GouveaHeitor         #
-# [+] TELEGRAM:     @GouveaHeitor                       #
-#########################################################
-
 package Nipe::Start;
 
 use Nipe::Device;

fb800ded74e673680a6bf2fa77a4500270c401df
Bruno Meneguele <bmeneguele@gmail.com>
2020-01-15 16:22:32 -0300

    install: remove redundant system commands
    Tor is the primary requirement for Nipe, thus it isn't necessary to `mkdir
    /etc/tor` directory in Nipe, since it's created by default on every distro
    Tor is properly installed.
    
    At the same time, the $operationalSystem var can be used to allow correct
    handling of which custom Nipe's config file should be copied to /etc/tor, thus
    I'm also removing the `cp` for every if statement, placing it at the end and
    making use of the already available mentioned var.

--- lib/Nipe/Install.pm
+++ lib/Nipe/Install.pm
@@ -7,28 +7,23 @@ use Nipe::Device;
 sub new {
 	my $operationalSystem = Nipe::Device -> getSystem();
 
-	system ("sudo mkdir -p /etc/tor");
-
 	if ($operationalSystem eq "debian") {
 		system ("sudo apt-get install tor iptables");
-		system ("sudo cp .configs/debian-torrc /etc/tor/torrc");
 	}
 	
 	elsif ($operationalSystem eq "fedora") {
 		system ("sudo dnf install tor iptables");
-		system ("sudo cp .configs/fedora-torrc /etc/tor/torrc");
 	}
 
 	elsif ($operationalSystem eq "centos") {
 		system ("sudo yum install epel-release tor iptables");
-		system ("sudo cp .configs/centos-torrc /etc/tor/torrc");
 	}
 
 	else {
 		system ("sudo pacman -S tor iptables");
-		system ("sudo cp .configs/arch-torrc /etc/tor/torrc");
 	}
 
+	system ("sudo cp .configs/$operationalSystem-torrc /etc/tor/torrc");
 	system ("sudo chmod 644 /etc/tor/torrc");
 
 	if (-e "/etc/init.d/tor") {

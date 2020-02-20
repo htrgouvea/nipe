9b6e7c6ca04b5f3f851c0be5532be7c3f1574a95
pest <mc.asdfgf@gmail.com>
2020-02-21 00:04:48 +0700

    few changes. experement  iptables-save-restore

--- lib/Nipe/Start.pm
+++ lib/Nipe/Start.pm
@@ -7,7 +7,7 @@ use Nipe::Device;
 sub new {
 	my $dnsPort      = "9061";
 	my $transferPort = "9051";
-	my @table        = ("nat", "filter");
+	# my @table        = ("nat", "filter");
 	my $network      = "10.66.0.0/255.255.0.0";
 
 	my $username = Nipe::Device -> getUsername();
@@ -19,52 +19,55 @@ sub new {
 	else {
 		system ("sudo systemctl start tor");
 	}
-	
-	foreach my $table (@table) {
-		my $target = "ACCEPT";
 
-		if ($table eq "nat") {
-			$target = "RETURN";
-		}
+    system ("sudo iptables-save > before_run.iptables");
+    system ("sudo iptables-restore < iptables_tor_rules.iptables");
+    
+	# foreach my $table (@table) {
+	# 	my $target = "ACCEPT";
 
-		system ("sudo iptables -t $table -F OUTPUT");
-		system ("sudo iptables -t $table -A OUTPUT -m state --state ESTABLISHED -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -m owner --uid $username -j $target");
+	# 	if ($table eq "nat") {
+	# 		$target = "RETURN";
+	# 	}
 
-		my $matchDnsPort = $dnsPort;
+	# 	system ("sudo iptables -t $table -F OUTPUT");
+	# 	system ("sudo iptables -t $table -A OUTPUT -m state --state ESTABLISHED -j $target");
+	# 	system ("sudo iptables -t $table -A OUTPUT -m owner --uid $username -j $target");
 
-		if ($table eq "nat") {
-			$target = "REDIRECT --to-ports $dnsPort";
-			$matchDnsPort = "53";
-		}
+	# 	my $matchDnsPort = $dnsPort;
 
-		system ("sudo iptables -t $table -A OUTPUT -p udp --dport $matchDnsPort -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -p tcp --dport $matchDnsPort -j $target");
+	# 	if ($table eq "nat") {
+	# 		$target = "REDIRECT --to-ports $dnsPort";
+	# 		$matchDnsPort = "53";
+	# 	}
 
-		if ($table eq "nat") {
-			$target = "REDIRECT --to-ports $transferPort";
-		}
+	# 	system ("sudo iptables -t $table -A OUTPUT -p udp --dport $matchDnsPort -j $target");
+	# 	system ("sudo iptables -t $table -A OUTPUT -p tcp --dport $matchDnsPort -j $target");
 
-		system ("sudo iptables -t $table -A OUTPUT -d $network -p tcp -j $target");
+	# 	if ($table eq "nat") {
+	# 		$target = "REDIRECT --to-ports $transferPort";
+	# 	}
 
-		if ($table eq "nat") {
-			$target = "RETURN";
-		}
+	# 	system ("sudo iptables -t $table -A OUTPUT -d $network -p tcp -j $target");
 
-		system ("sudo iptables -t $table -A OUTPUT -d 127.0.0.1/8    -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -d 192.168.0.0/16 -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -d 172.16.0.0/12  -j $target");
-		system ("sudo iptables -t $table -A OUTPUT -d 10.0.0.0/8     -j $target");
+	# 	if ($table eq "nat") {
+	# 		$target = "RETURN";
+	# 	}
 
-		if ($table eq "nat") {
-			$target = "REDIRECT --to-ports $transferPort";
-		}
+	# 	system ("sudo iptables -t $table -A OUTPUT -d 127.0.0.1/8    -j $target");
+	# 	system ("sudo iptables -t $table -A OUTPUT -d 192.168.0.0/16 -j $target");
+	# 	system ("sudo iptables -t $table -A OUTPUT -d 172.16.0.0/12  -j $target");
+	# 	system ("sudo iptables -t $table -A OUTPUT -d 10.0.0.0/8     -j $target");
 
-		system ("sudo iptables -t $table -A OUTPUT -p tcp -j $target");
-	}
+	# 	if ($table eq "nat") {
+	# 		$target = "REDIRECT --to-ports $transferPort";
+	# 	}
 
-	system ("sudo iptables -t filter -A OUTPUT -p udp -j REJECT");
-	system ("sudo iptables -t filter -A OUTPUT -p icmp -j REJECT");
+	# 	system ("sudo iptables -t $table -A OUTPUT -p tcp -j $target");
+	# }
+
+	# system ("sudo iptables -t filter -A OUTPUT -p udp -j REJECT");
+	# system ("sudo iptables -t filter -A OUTPUT -p icmp -j REJECT");
 
 	return true;
 }
--- lib/Nipe/Stop.pm
+++ lib/Nipe/Stop.pm
@@ -5,10 +5,11 @@ package Nipe::Stop;
 sub new {
 	my @table = ("nat", "filter");
 
-	foreach my $table (@table) {
-		system ("sudo iptables -t $table -F OUTPUT");
-		system ("sudo iptables -t $table -F OUTPUT");
-	}
+	# foreach my $table (@table) {
+	# 	system ("sudo iptables -t $table -F OUTPUT");
+	# 	system ("sudo iptables -t $table -F OUTPUT");
+	# }
+    system ("sudo iptables-restore < before_run.iptables");
 
 	if (-e "/etc/init.d/tor") {
 		system ("sudo /etc/init.d/tor stop > /dev/null");

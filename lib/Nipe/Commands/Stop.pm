0c227100187efd8c3362bded9a6b55c21f666904
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2020-02-21 20:04:44 -0300

    Update Stop sub command file fixing code format

--- lib/Nipe/Commands/Stop.pm
+++ lib/Nipe/Commands/Stop.pm
@@ -2,29 +2,39 @@ package Nipe::Commands::Stop;
 use strict;
 use warnings;
 
+use Nipe::Device;
+
 sub run {
   my ($self, $arguments) = @_;
+
+  my $user      = Nipe::Device->tor_username;
+  my $lock_file = "/var/run/nipe/instance.lock";
 
   my $options = _parse_arguments($arguments);
 
   for my $table ($options->{table}) {
-		system ("sudo iptables -t $table -F OUTPUT");
-		system ("sudo iptables -t $table -F OUTPUT");
+    system "sudo iptables -t $table -F OUTPUT";
+    system "sudo iptables -t $table -F OUTPUT";
   }
 
-	my $tor_command = (-e "/etc/init.d/tor") ?
-		"/etc/init.d/tor stop > /dev/null" :
-    "systemctl stop tor";
+  my $nipe_pid           = `sudo -u $user cat $lock_file 2>&1`;
+  my $nipe_command_error = $? != 0;
+  chomp($nipe_pid);
 
-	system("sudo $tor_command");
+  if ($nipe_command_error) {
+    print "[.] No instance of tor executed by Nipe was found\n";
+    return;
+  }
+
+  system "sudo -u $user kill -SIGINT $nipe_pid > /dev/null";
+  system "sudo -u $user rm -f $lock_file > /dev/null";
+  print "[.] Tor instance with PID=$nipe_pid terminated\n";
 }
 
 sub _parse_arguments {
   my ($arguments) = @_;
 
-  my $options = {
-	  table => ["nat", "filter"],
-  };
+  my $options = {table => ["nat", "filter"],};
 
   return $options;
 }

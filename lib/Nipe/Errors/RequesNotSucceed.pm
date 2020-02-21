97ae3976c51ce096287e58d441e7ef3a0b78178c
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2020-02-21 08:37:35 -0300

    Add a new namespace for error classes

---
+++ lib/Nipe/Errors/RequesNotSucceed.pm
@@ -0,0 +1,38 @@
+package Nipe::Errors::RequesNotSucceed;
+use strict;
+use warnings;
+
+use Scalar::Util qw(blessed);
+
+use overload '""' => sub { $_[0]->to_string };
+
+sub new {
+  my ($class, $args) = (shift, {@_});
+  return bless $args || {}, $class;
+}
+
+sub throws {
+  my ($self, $status, $message) = @_;
+
+  $self = $self->new(status => $status, message => $message)
+    unless blessed $self;
+
+  die $self;
+}
+
+sub status {
+  my ($self) = @_;
+  return $self->{status};
+}
+
+sub message {
+  my ($self) = @_;
+  return $self->{message};
+}
+
+sub to_string {
+  my $self = shift;
+  return $self->{message};
+}
+
+1;

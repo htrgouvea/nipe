b4c51792d4dd6c95d532487c57212fb8e873881c
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2020-02-21 08:39:40 -0300

    Update nipe.pl to use new command dispatcher implementation

--- nipe.pl
+++ nipe.pl
@@ -1,37 +1,10 @@
 #!/usr/bin/env perl
 
 use 5.018;
-use Switch;
+use strict;
+use warnings;
+
 use lib "./lib/";
-use Nipe::Stop;
-use Nipe::Start;
-use Nipe::CheckIp;
-use Nipe::Functions;
+use Nipe::Commands;
 
-sub main {
-	my $command = $ARGV[0];
-
-	switch ($command) {
-		case "stop" {
-			Nipe::Stop -> new();
-		}
-		case "start" {
-			Nipe::Start -> new();
-		}
-		case "status" {
-			Nipe::CheckIp -> new();
-		}
-		case "restart" {
-			Nipe::Stop -> new();
-			Nipe::Start -> new();
-		}
-		case "install" {
-			Nipe::Functions -> install();
-		}
-
-		Nipe::Functions -> help();
-	}
-}
-
-main();
-exit;
+Nipe::Commands->run(\@ARGV);

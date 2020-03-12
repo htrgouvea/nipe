2f18ca9a63e44098a7c08b907e14caf46829181d
GouveaHeitor <hi@heitorgouvea.me>
2020-03-12 09:47:48 -0300

    adjust help menu

--- lib/Nipe/Helper.pm
+++ lib/Nipe/Helper.pm
@@ -10,10 +10,7 @@ sub new {
 		\r\tCommand       Description
 		\r\t-------       -----------
 		\r\tinstall       Install dependencies
-		\r\t  -f          Overwrite Tor config file in /etc/tor/torrc
-		\r\t  -c <file>   Specify a custom location to install Tor's config file
 		\r\tstart         Start routing
-		\r\t  -c <file>   Specify a custom Tor config file to be used by Nipe
 		\r\tstop          Stop routing
 		\r\trestart       Restart the Nipe process
 		\r\tstatus        See status

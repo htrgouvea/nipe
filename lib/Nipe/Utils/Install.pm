aa8ee145f22fa45f89c833fd2bd89888d98718fc
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2020-08-15 15:00:22 +0200

    check if AppArmor is enabled and allow Tor read nipe config

--- lib/Nipe/Utils/Install.pm
+++ lib/Nipe/Utils/Install.pm
@@ -7,7 +7,6 @@ package Nipe::Utils::Install {
 
 	sub new {
 		my %device  = Nipe::Utils::Device -> new();
-		my $stopTor = "systemctl stop tor";
 		my $nipeTorConfig = "/etc/torrc.d/nipe";
 
 		if (! -d dirname ($nipeTorConfig)) {
@@ -18,11 +17,25 @@ package Nipe::Utils::Install {
 			copy ("torrc.d/nipe", $nipeTorConfig) or die "Copy failed: $!";
 		}
 
+		if (! -d dirname ($nipeTorConfig)) {
+			mkdir dirname ($nipeTorConfig), 0755;
+		}
+
 		open (FILE, "+<", "/etc/tor/torrc");
 		if (not grep{ /^%include \/etc\/torrc\.d\/nipe$/ } <FILE>) {
 			print FILE "%include /etc/torrc.d/nipe\n";
 		}
 		close FILE;
+
+		# find out if AppArmor is enabled (returns Y if true)
+		open FILE, "<", "/sys/module/apparmor/parameters/enabled";
+		chomp(my $apparmor = <FILE>);
+		close FILE;
+		if ( $apparmor eq "Y" ) {
+			open APPARMOR, ">", "/etc/apparmor.d/local/system_tor";
+			print APPARMOR "/etc/torrc.d/nipe r,\n";
+			close APPARMOR;
+		}
 
 		if ($device{distribution} eq "debian") {
 			system ("apt-get install -y tor iptables");

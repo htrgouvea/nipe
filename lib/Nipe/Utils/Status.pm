1bfc22083309b69017f6a4966942b305aebb3195
htrgouvea <hi@heitorgouvea.me>
2022-07-05 08:39:50 -0300

    fix grammar

--- lib/Nipe/Utils/Status.pm
+++ lib/Nipe/Utils/Status.pm
@@ -14,7 +14,7 @@ package Nipe::Utils::Status {
 			my $checkIp  = $data -> {"IP"};
 			my $checkTor = $data -> {"IsTor"} ? "true" : "false";
 
-			return "\n\r[+] Status: $checkTor. \n\r[+] Ip: $checkIp\n\n";
+			return "\n\r[+] Status: $checkTor \n\r[+] Ip: $checkIp\n\n";
 		}
 
 		return "\n[!] ERROR: sorry, it was not possible to establish a connection to the server.\n\n";
